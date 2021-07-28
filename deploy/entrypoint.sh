6ca6b77e6aa5440005bacb2ed3df7001f5bdcc96
Aaron Thompson <thompson@snapstream.com>
2021-07-28 17:36:42 -0500

    images

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 : ${AWS_REGION:=us-east-1}
-: ${DOTNET_LAMBDA_PACKAGE_NAME:=latest.zip}
 
 set -x
 
 export PATH="$PATH:/root/.dotnet/tools"
 cd "${DOTNET_LAMBDA_WORKING_DIR:-.}"
-dotnet lambda package $DOTNET_LAMBDA_PACKAGE_NAME
 
-aws lambda update-function-code \
-  --region $AWS_REGION \
-  --function-name $DOTNET_LAMBDA_FUNCTION_NAME \
-  --zip-file fileb://$DOTNET_LAMBDA_PACKAGE_NAME
 dotnet lambda deploy-function \
-  --config-file $DOTNET_LAMBDA_CONFIG_FILE \
   --region $AWS_REGION \
-  --function-name $DOTNET_LAMBDA_FUNCTION_NAME \
-  --function-handler $DOTNET_LAMBDA_FUNCTION_HANDLER \
-  --package $DOTNET_LAMBDA_PACKAGE_NAME
-exit
+  --aws-access-key-id ${{ secrets.AWS_ACCESS_KEY_ID }} \
+  --aws-secret-key ${{ secrets.AWS_SECRET_ACCESS_KEY }}
